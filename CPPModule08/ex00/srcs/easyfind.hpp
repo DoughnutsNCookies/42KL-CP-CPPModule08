b611572538ba760e5b079d37fb081d5d658f3572
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2022-09-06 18:43:41 +0800

    Finished ex01

--- CPPModule08/ex00/srcs/easyfind.hpp
+++ CPPModule08/ex00/srcs/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 18:40:49 by schuah            #+#    #+#             */
-/*   Updated: 2022/09/05 20:01:46 by schuah           ###   ########.fr       */
+/*   Updated: 2022/09/06 14:16:10 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,21 @@
 # include <algorithm>
 # include <iostream>
 # include <vector>
-# include <list>
- 
+
+/* Container.begin() points to the first element of the vector
+** Container.end() points to the last element of the vector
+** Std::find returns the element if it is found inside the vector,
+** else returns the end instead */
 template<typename T>
-typename T::iterator	easyfind(T &container, const int tofind)
+typename T::iterator	easyfind(T &container, const int toFind)
 {
-	return (std::find(container.begin(), container.end(), tofind));
+	return (std::find(container.begin(), container.end(), toFind));
 }
 
 template<typename T>
-typename T::const_iterator	easyfind(const T &container, const int tofind)
+typename T::const_iterator	easyfind(const T &container, const int toFind)
 {
-	return (std::find(container.begin(), container.end(), tofind));
+	return (std::find(container.begin(), container.end(), toFind));
 }
 
 #endif
--- CPPModule08/ex00/srcs/main.cpp
+++ CPPModule08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 18:40:04 by schuah            #+#    #+#             */
-/*   Updated: 2022/09/05 20:26:27 by schuah           ###   ########.fr       */
+/*   Updated: 2022/09/06 14:16:49 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,20 @@ int main(int ac, char **av)
 	const int				array2[5] = {0, -1, -100, 23, 88};
 	std::vector<int>		intVector1(array1, array1 + 5);
 	const std::vector<int>	intVector2(array2, array2 + 5);
-	const int 				tofind = std::atoi(av[1]);
+	int 					toFind; 
 
 	if (ac != 2)
 	{
 		std::cout << "./easyfind [ToFind]" << std::endl;
 		return (0);
 	}
-	std::vector<int>::iterator number1 = easyfind(intVector1, tofind);
+	toFind = std::atoi(av[1]);
+	std::vector<int>::iterator number1 = easyfind(intVector1, toFind);
 	if (number1 != intVector1.end())
 		std::cout << "Found " << *number1 << " in intVector1"<< std::endl;
 	else
 		std::cout << "Not found in intVector1" << std::endl;
-	std::vector<int>::const_iterator number2 = easyfind(intVector2, tofind);
+	std::vector<int>::const_iterator number2 = easyfind(intVector2, toFind);
 	if (number2 != intVector2.end())
 		std::cout << "Found " << *number2 << " in intVector2"<< std::endl;
 	else
